a3030d1291397fbcc9a0b190c7e092b18b293fec
Abdullah Mehmood <am24abp@herts.ac.uk>
2024-11-22 21:38:57 +0000

    Convert the average participation into numbers and remove NA values.

--- A111_Assignment_01.R
+++ A111_Assignment_01.R
@@ -6,3 +6,8 @@ data <- read_csv("Pregnant_Women_Participating.csv")
 #show dataset
 data
 
+# convert average participant to number
+data$Average.Participation <- as.numeric(data$Average.Participation)
+
+#Now i want to remove NA values if have any in dataset
+average_participation <- na.omit(data$Average.Participation)
